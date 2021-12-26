c0b224527cf91a35968a6787e3cc574fc6b07ef6
ishaandwivedi1234 <ishaan.dwivedi99@gmail.com>
2021-12-27 00:09:48 +0530

    finally

--- clean-it-frontend/cleanit/lib/services/apiService.dart
+++ clean-it-frontend/cleanit/lib/services/apiService.dart
@@ -5,26 +5,28 @@ import 'package:cleanit/services/modelHelper.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+
 class ApiService {
   final ModelHelper modelHelper = new ModelHelper();
-  Future<Map<String, dynamic>> registerUser(User user, String password) {
-    // try {
-    //   var body = jsonEncode(
-    //       modelHelper.getRegisterUserBody(user.email,password,user.phone);
-    //   var urlString = apiConfig.baseUrl + apiConfig.addPasswordUrl;
-    //   var url = Uri.parse(urlString);
-    //   final response = await http.post(url,
-    //       headers: apiConfig.getHeaders(appUser: appUser), body: body);
+  Future<Map<String, dynamic>> registerUser(User user,String password) {
+    try {
+      var body = jsonEncode(
+          modelHelper.getRegisterUserBody(user.email,password,user.phone);
+      var urlString = apiConfig.baseUrl + apiConfig.addPasswordUrl;
+      var url = Uri.parse(urlString);
+      final response = await http.post(url,
+          headers: apiConfig.getHeaders(appUser: appUser), body: body);
 
-    //   // print(response.body);
+      // print(response.body);
 
-    //     var apiResponse = {"password": savedPassword, "error": null};
-    //     return apiResponse;
-    //   } else {
-
-    //   }
-    // } on HttpException catch (e) {
-
-    // }
+     
+        var apiResponse = {"password": savedPassword, "error": null};
+        return apiResponse;
+      } else {
+        
+      }
+    } on HttpException catch (e) {
+    
+    }
   }
 }
